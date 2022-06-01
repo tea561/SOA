08c921dd589ca8a0d1bbfc5ee61989dfabbae7b6
tea561 <stamenkovicteodora99@gmail.com>
2022-06-01 14:17:54 +0200

    Subscribe na topic

--- Projekti/Projekat/services/analytics/app.py
+++ Projekti/Projekat/services/analytics/app.py
@@ -4,22 +4,29 @@ import paho.mqtt.client as mqtt
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code "+str(rc), flush = True)
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+    client.subscribe("projekat/vitals")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(msg.topic+" "+str(msg.payload), flush = True)
+
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print("Unexpected disconnection.", flush = True)
 
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_disconnect = on_disconnect
 
-client.connect("172.21.0.4", 1883, 60)
+print("Trying to connect", flush = True)
+client.connect("mqtt", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
